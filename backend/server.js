7fb1dbde3e5a59fa79d82fc88efe5b3531be668d
natinan <natinan.ra.63@ubu.ac.th>
2022-12-20 17:26:47 +0700

    api update method

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var cors = require('cors')
 const mysql = require('mysql2');
+const { Message } = require('@mui/icons-material');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -33,7 +34,7 @@ app.use(express.json())
 //   next();
 // });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -92,9 +93,97 @@ app.delete('/users/del', function (req, res, next) {
     'DELETE FROM `employees` WHERE id = ?',
     [req.body.id],
     function (err, results) {
-    return res.json(results)
-     
-      
+      return res.json(results)
+
+
+    }
+  );
+})
+//update type 1
+// app.put('users/update', function (req, res, next) {
+//   let id = req.body.id
+//   let number_id = req.body.number_id
+//   let fname = req.body.fname
+//   let lname = req.body.lname
+//   let start_date = req.body.start_date
+//   let date_of_birth = req.body.date_of_birth
+//   let age = req.body.age
+//   let sex = req.body.sex
+//   let height = req.body.height
+//   let weight = req.body.weight
+//   let blood = req.body.blood
+
+//   // if (!id || !number_id || !fname || !lname || !start_date || !date_of_birth || !age || !height || !weight || !blood||!id) {
+//   //   return res.status(400)
+//   //     .send({
+//   //       error: true,
+//   //       message:'Data is not completely '
+//   //     });
+//   // }else{
+//     connection.query(
+//       'UPDATE `employees` SET `number_id` = ?, `fname` = ?, `lname` = ?, `start_date` = ?, `date_of_birth` = ?, `age` = ?, `sex` = ?, `height` = ?,`weight` = ?, `blood` = ? WHERE id = ?',
+//       // [number_id,fname,lname,start_date,date_of_birth,age,sex,height,weight,blood,id],
+//       [
+//         req.body.number_id,
+//          req.body.fname, 
+//          req.body.lname, 
+//          req.body.start_date, 
+//          req.body.date_of_birth, 
+//          req.body.age, 
+//          req.body.sex, 
+//          req.body.height, 
+//          req.body.weight, 
+//          req.body.blood,
+//          req.body.id
+//         ],
+//       function (err,results) {
+//         res.json(results)
+//         // if (error) throw error;
+//         // let message = ""
+//         // if (result.changedRows === 0) {
+//         //   message = "not update"
+//         // }
+//         // else{
+//         //   message = "Update is successfully"
+//         // }
+
+//         // return res.send({
+//         //   error:false,data:result,message:message
+//         // })
+        
+//       }
+//     )
+//   // }
+//   // connection.query(
+//   //   'INSERT INTO `employees` ( `number_id`, `fname`, `lname`, `start_date`, `date_of_birth`, `age`, `sex`, `height`, `weight`, `blood`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?,?)  WHERE `id` = ? ',
+//   //   [req.body.number_id, req.body.fname, req.body.lname, req.body.start_date, req.body.date_of_birth, req.body.age, req.body.sex, req.body.height, req.body.weight, req.body.blood,id],
+//   //   function (err, results) {
+//   //      res.json(results,err);
+//   //     res.json('Create user successfully')
+//   //   }
+//   // )
+
+// })
+
+// update 2 
+app.put('/users/update', function (req, res, next) {
+  connection.query(
+    'UPDATE  `employees` SET `number_id` = ?, `fname` = ?, `lname` = ?, `start_date` = ?, `date_of_birth` = ?, `age` = ?, `sex` = ?, `height` = ?,`weight` = ?, `blood` = ? WHERE id = ?',
+    [
+      req.body.number_id,
+      req.body.fname,
+      req.body.lname,
+      req.body.start_date,
+      req.body.date_of_birth,
+      req.body.age,
+      req.body.sex,
+      req.body.height,
+      req.body.weight,
+      req.body.blood,
+      req.body.id
+    ],
+    function(err, results) {
+      res.json(results);
     }
   );
 })
