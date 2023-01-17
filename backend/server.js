cdae3e7a4a2fefa1d48e5d706a7c0e1e466004fb
natinan <natinan.ra.63@ubu.ac.th>
2023-01-17 22:31:08 +0700

    add login all user

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-var express = require('express')
-var cors = require('cors')
-const mysql = require('mysql2');
-const { Message } = require('@mui/icons-material');
+var express = require("express");
+var cors = require("cors");
+const mysql = require("mysql2");
+const { Message } = require("@mui/icons-material");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'mydb',
-  password: 'root'
+  host: "localhost",
+  user: "root",
+  database: "mydb",
+  password: "root",
 });
 
-var app = express()
-app.use(cors())
-app.use(express.json())
+var app = express();
+app.use(cors());
+app.use(express.json());
 
 // Add headers before the routes are defined
 // app.use(function (req, res, next) {
@@ -36,69 +36,116 @@ app.use(express.json())
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 app.listen(5000, function () {
-  console.log('CORS-enabled web server listening on port 5000')
-})
+  console.log("CORS-enabled web server listening on port 5000");
+});
 
-
-
-app.get('/users', function (req, res, next) {
-
+app.post("/login", function (req, res) {
   connection.query(
-    'SELECT * FROM `employees`',
+    "SELECT * FROM `doctors` WHERE `fname` = ? AND `password` = ?  ",
+    // 'SELECT * FROM (  SELECT * FROM `employees` WHERE `fname` = ? AND WHERE `password` = ? UNION ALL SELECT * FROM `doctors` WHERE `fname` = ? AND WHERE `password` = ?) WHERE condition',
+    [req.body.username, req.body.password],
+    function (err, result) {
+      if (result.length > 0) {
+        res.json(result);
+      } else if (result <= 0) {
+        connection.query(
+          "SELECT * FROM `employees` WHERE `number_id` = ? AND `password` = ?  ",
+          [req.body.username, req.body.password],
+          function (err2, result2) {
+            if (result2.length > 0) {
+              res.json(result2);
+            } 
+            else if (result2 <= 0) {
+              connection.query(
+                "SELECT * FROM `ower` WHERE `number_id` = ? AND `password` = ?  ",
+                [req.body.username, req.body.password],
+                function (err3,result3) {
+                  if (result3.length > 0) {
+                     res.json(result3)
+                  }
+                  else{
+                    res.json('User not found')
+                  }
+                }
+              );
+            }
+            else{
+              res.json('user not found 1')
+            }
+          }
+        );
+      }
+    }
+  );
+});
+
+//employees methods
+app.get("/users", function (req, res, next) {
+  connection.query(
+    "SELECT * FROM `employees`",
     function (err, results, fields) {
       res.json(results);
     }
   );
-})
+});
 
-app.get('/users/:id', function (req, res, next) {
-
+app.get("/users/:id", function (req, res, next) {
   const id = req.params.id;
   connection.query(
-    'SELECT * FROM `employees` WHERE `id` = ?',
+    "SELECT * FROM `employees` WHERE `id` = ?",
     [id],
     function (err, results) {
       if (!results) {
-        res.json(null)
+        res.json(null);
       } else {
         res.json(results);
       }
-
     }
   );
-})
+});
 
-app.post('/users/add', function (req, res, next) {
-
+app.post("/users/add", function (req, res, next) {
   // res.header( "Access-Control-Allow-Origin" );
   connection.query(
-    'INSERT INTO `employees` ( `number_id`, `fname`, `lname`, `start_date`, `date_of_birth`, `age`, `sex`, `height`, `weight`, `blood`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?,?)',
-    [req.body.number_id, req.body.fname, req.body.lname, req.body.start_date, req.body.date_of_birth, req.body.age, req.body.sex, req.body.height, req.body.weight, req.body.blood],
+    "INSERT INTO `employees` ( `number_id`, `fname`, `lname`, `start_date`, `date_of_birth`, `age`, `sex`, `height`, `weight`, `blood`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?,?)",
+    [
+      req.body.number_id,
+      req.body.fname,
+      req.body.lname,
+      req.body.start_date,
+      req.body.date_of_birth,
+      req.body.age,
+      req.body.sex,
+      req.body.height,
+      req.body.weight,
+      req.body.blood,
+    ],
     function (err, results) {
       // res.json(results);
-      res.json('Create user successfully')
+      res.json("Create user successfully");
     }
   );
-})
+});
 
-app.delete('/users/del', function (req, res, next) {
+app.delete("/users/del", function (req, res, next) {
   // res.header( "Access-Control-Allow-Origin", );
 
   connection.query(
-    'DELETE FROM `employees` WHERE id = ?',
+    "DELETE FROM `employees` WHERE id = ?",
     [req.body.id],
     function (err, results) {
-      return res.json(results)
-
-
+      return res.json(results);
     }
   );
-})
+});
 
 //update type 1
 // app.put('users/update', function (req, res, next) {
@@ -126,14 +173,14 @@ app.delete('/users/del', function (req, res, next) {
 //       // [number_id,fname,lname,start_date,date_of_birth,age,sex,height,weight,blood,id],
 //       [
 //         req.body.number_id,
-//          req.body.fname, 
-//          req.body.lname, 
-//          req.body.start_date, 
-//          req.body.date_of_birth, 
-//          req.body.age, 
-//          req.body.sex, 
-//          req.body.height, 
-//          req.body.weight, 
+//          req.body.fname,
+//          req.body.lname,
+//          req.body.start_date,
+//          req.body.date_of_birth,
+//          req.body.age,
+//          req.body.sex,
+//          req.body.height,
+//          req.body.weight,
 //          req.body.blood,
 //          req.body.id
 //         ],
@@ -166,10 +213,10 @@ app.delete('/users/del', function (req, res, next) {
 
 // })
 
-// update 2 
-app.put('/users/update', function (req, res, next) {
+// update 2
+app.put("/users/update", function (req, res, next) {
   connection.query(
-    'UPDATE  `employees` SET `number_id` = ?, `fname` = ?, `lname` = ?, `start_date` = ?, `date_of_birth` = ?, `age` = ?, `sex` = ?, `height` = ?,`weight` = ?, `blood` = ? WHERE id = ?',
+    "UPDATE  `employees` SET `number_id` = ?, `fname` = ?, `lname` = ?, `start_date` = ?, `date_of_birth` = ?, `age` = ?, `sex` = ?, `height` = ?,`weight` = ?, `blood` = ? WHERE id = ?",
     [
       req.body.number_id,
       req.body.fname,
@@ -181,78 +228,74 @@ app.put('/users/update', function (req, res, next) {
       req.body.height,
       req.body.weight,
       req.body.blood,
-      req.body.id
+      req.body.id,
     ],
     function (err, results) {
       res.json(results);
     }
   );
-})
+});
 
 //------------------------------customer---------------------------------//
 //add customer
-app.post('/customer/add', function (req, res, next) {
-connection.query(
-  'INSERT INTO `customers` ( `number_id`, `fname`, `lname`, `phone_number`, `sex`, `blood`,`drug_allergy`,`congenital_disease`,`etc_note`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    [req.body.number_id, 
-      req.body.fname, 
-      req.body.lname, 
-      req.body.phone_number, 
-      req.body.sex, 
-      req.body.blood, 
-      req.body.drug_allergy, 
-      req.body.congenital_diseaset, 
-      req.body.etc_note],
+app.post("/customer/add", function (req, res, next) {
+  connection.query(
+    "INSERT INTO `customers` ( `number_id`, `fname`, `lname`, `phone_number`, `sex`, `blood`,`drug_allergy`,`congenital_disease`,`etc_note`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      req.body.number_id,
+      req.body.fname,
+      req.body.lname,
+      req.body.phone_number,
+      req.body.sex,
+      req.body.blood,
+      req.body.drug_allergy,
+      req.body.congenital_diseaset,
+      req.body.etc_note,
+    ],
     function (err, results) {
       res.json(results);
     }
-)
-})
+  );
+});
 
 //get all customers
-app.get('/customer',function(req,res,next){
-  connection.query(
-    'SELECT * FROM `customers`',
-    function (err,result,field) {
-      res.json(result)
-    }
-  )
-})
+app.get("/customer", function (req, res, next) {
+  connection.query("SELECT * FROM `customers`", function (err, result, field) {
+    res.json(result);
+  });
+});
 
 //delete customer
-app.delete('/customer/delete',function (req,res,next) {
+app.delete("/customer/delete", function (req, res, next) {
   connection.query(
-    'DELETE FROM `customers` WHERE number_id = ?',
+    "DELETE FROM `customers` WHERE number_id = ?",
     [req.body.number_id],
-    function (err,result) {
-      res.json(result)
+    function (err, result) {
+      res.json(result);
     }
-  )
-  
-})
+  );
+});
 
 //get some customer
-app.get('/customer/:id', function (req, res, next) {
-
+app.get("/customer/:id", function (req, res, next) {
   const id = req.params.id;
   connection.query(
-    'SELECT * FROM `customers` WHERE `number_id` = ?',
+    "SELECT * FROM `customers` WHERE `number_id` = ?",
     [id],
     function (err, results) {
       if (!results) {
-        res.json(null)
+        res.json(null);
       } else {
         res.json(results);
       }
-
     }
   );
-})
+});
 
 //update customer
-app.put('/customers/edit', function (req, res, next) {
+app.put("/customers/edit", function (req, res, next) {
   connection.query(
-    'UPDATE  `customers` SET `fname` = ?, `lname` = ?, `phone_number` = ?, `sex` = ?, `blood` = ?, `drugAllergy` = ?, `congenitalDisease` = ?,`etcNote` = ?  WHERE number_id = ?',
+    "UPDATE  `customers` SET `fname` = ?, `lname` = ?, `phone_number` = ?, `sex` = ?, `blood` = ?, `drugAllergy` = ?, `congenitalDisease` = ?,`etcNote` = ?  WHERE number_id = ?",
     [
       req.body.fname,
       req.body.lname,
@@ -262,96 +305,75 @@ app.put('/customers/edit', function (req, res, next) {
       req.body.drug_allergy,
       req.body.congenital_disease,
       req.body.etc_note,
-      req.body.number_id
+      req.body.number_id,
     ],
     function (err, results) {
       if (results) {
         res.json(results);
+      } else {
+        res.json(err);
       }
-      else{
-        res.json(err)
-      }
-      
     }
   );
-})
+});
 
 //------------------------------doctor-------------------------------//
 //add doctor
-app.post('/doctor/add', function (req, res, next) {
+app.post("/doctor/add", function (req, res, next) {
   connection.query(
-    'INSERT INTO `doctors` (  `fname`, `lname`, `password`) VALUES ( ?, ?, ?)',
-      [
-        req.body.fname, 
-        req.body.lname, 
-        req.body.password
-      ],
-      function (err, results) {
+    "INSERT INTO `doctors` (  `fname`, `lname`, `password`) VALUES ( ?, ?, ?)",
+    [req.body.fname, req.body.lname, req.body.password],
+    function (err, results) {
+      res.json(results);
+    }
+  );
+});
+
+//get all doctors
+app.get("/doctor", function (req, res, next) {
+  connection.query("SELECT * FROM `doctors`", function (err, result, field) {
+    res.json(result);
+  });
+});
+
+//delete doctor
+app.delete("/doctor/delete", function (req, res, next) {
+  connection.query(
+    "DELETE FROM `doctors` WHERE id = ?",
+    [req.body.id],
+    function (err, result) {
+      res.json(result);
+    }
+  );
+});
+
+//get some doctor
+app.get("/doctor/:id", function (req, res, next) {
+  const id = req.params.id;
+  connection.query(
+    "SELECT * FROM `doctors` WHERE `id` = ?",
+    [id],
+    function (err, results) {
+      if (!results) {
+        res.json(null);
+      } else {
         res.json(results);
       }
-  )
-  })
-  
-  //get all doctors
-  app.get('/doctor',function(req,res,next){
-    connection.query(
-      'SELECT * FROM `doctors`',
-      function (err,result,field) {
-        res.json(result)
-      }
-    )
-  })
-  
-  //delete doctor
-  app.delete('/doctor/delete',function (req,res,next) {
-    connection.query(
-      'DELETE FROM `doctors` WHERE id = ?',
-      [req.body.id],
-      function (err,result) {
-        res.json(result)
-      }
-    )
-    
-  })
-  
-  //get some doctor
-  app.get('/doctor/:id', function (req, res, next) {
-  
-    const id = req.params.id;
-    connection.query(
-      'SELECT * FROM `doctors` WHERE `id` = ?',
-      [id],
-      function (err, results) {
-        if (!results) {
-          res.json(null)
-        } else {
-          res.json(results);
-        }
-  
-      }
-    );
-  })
-  
-  //update doctor
-  app.put('/doctor/edit', function (req, res, next) {
-    connection.query(
-      'UPDATE  `doctors` SET `fname` = ?, `lname` = ?  WHERE id = ?',
-      [
-        req.body.fname,
-        req.body.lname,
-        req.body.id
-      ],
-      function (err, results) {
-        if (results) {
-          res.json(results);
-        }
-        else{
-          res.json(err)
-        }
-        
-      }
-    );
-  })
+    }
+  );
+});
 
-
-
+//update doctor
+app.put("/doctor/edit", function (req, res, next) {
+  connection.query(
+    "UPDATE  `doctors` SET `fname` = ?, `lname` = ?  WHERE id = ?",
+    [req.body.fname, req.body.lname, req.body.id],
+    function (err, results) {
+      if (results) {
+        res.json(results);
+      } else {
+        res.json(err);
+      }
+    }
+  );
+});
